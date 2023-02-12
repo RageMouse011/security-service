550d467ccfe5fde0a8af6bde598a3928f04dcb05
daniil <zinindaniil11@gmail.com>
2023-02-12 11:24:47 +0600

    Removed: var, Optional, ResponseEntity.

--- src/main/java/kz/dar/tech/securityservice/config/ApplicationConfiguration.java
+++ src/main/java/kz/dar/tech/securityservice/config/ApplicationConfiguration.java
@@ -32,8 +32,7 @@ public class ApplicationConfiguration {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration config) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
 
